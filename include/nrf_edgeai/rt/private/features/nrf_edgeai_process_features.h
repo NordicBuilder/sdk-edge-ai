efd96e72f7496575cf8157d7f080207ce6c606a4
Raman Rusak <raman.rusak@nordicsemi.no>
2026-09-22 15:10:10 +0400

    lib: nrf_edgeai: styling for add-on codestyle
    styling: line 100 column, pointer align ->right, empty line EOF, clean tralling spaces
    Signed-off-by: Raman Rusak <raman.rusak@nordicsemi.no>

--- include/nrf_edgeai/rt/private/features/nrf_edgeai_process_features.h
+++ include/nrf_edgeai/rt/private/features/nrf_edgeai_process_features.h
@@ -15,29 +15,30 @@ extern "C" {
 /** @name DSP Feature Extraction
  *  @brief Extract DSP features from input data.
  *
- *  These functions perform digital signal processing (DSP) feature extraction (such as statistical or spectral features)
- *  from the input data.
- *  The extraction is performed according to the configuration in the neural network context.
- * 
+ * These functions perform digital signal processing (DSP) feature extraction (such as statistical
+ * or spectral features) from the input data. The extraction is performed according to the
+ * configuration in the neural network context.
+ *
  * @note For models that do not require feature extraction, an empty implementation is provided.
  *
  *  @param[in, out] p_input        Pointer to the input processing context @ref nrf_edgeai_input_t
- *  @param[in, out] p_dsp          Pointer to the DSP pipeline context @ref nrf_edgeai_dsp_pipeline_t
+ *  @param[in, out] p_dsp          Pointer to the DSP pipeline context @ref
+ * nrf_edgeai_dsp_pipeline_t
  *  @return Status code indicating success or error.
  * @{
  */
 
-nrf_edgeai_err_t nrf_edgeai_process_features_dsp_i8(nrf_edgeai_input_t*        p_input,
-                                                    nrf_edgeai_dsp_pipeline_t* p_dsp);
+nrf_edgeai_err_t nrf_edgeai_process_features_dsp_i8(nrf_edgeai_input_t	      *p_input,
+						    nrf_edgeai_dsp_pipeline_t *p_dsp);
 
-nrf_edgeai_err_t nrf_edgeai_process_features_dsp_i16(nrf_edgeai_input_t*        p_input,
-                                                     nrf_edgeai_dsp_pipeline_t* p_dsp);
+nrf_edgeai_err_t nrf_edgeai_process_features_dsp_i16(nrf_edgeai_input_t	       *p_input,
+						     nrf_edgeai_dsp_pipeline_t *p_dsp);
 
-nrf_edgeai_err_t nrf_edgeai_process_features_dsp_f32(nrf_edgeai_input_t*        p_input,
-                                                     nrf_edgeai_dsp_pipeline_t* p_dsp);
+nrf_edgeai_err_t nrf_edgeai_process_features_dsp_f32(nrf_edgeai_input_t	       *p_input,
+						     nrf_edgeai_dsp_pipeline_t *p_dsp);
 
-nrf_edgeai_err_t nrf_edgeai_process_features_empty(nrf_edgeai_input_t*        p_input,
-                                                   nrf_edgeai_dsp_pipeline_t* p_dsp);
+nrf_edgeai_err_t nrf_edgeai_process_features_empty(nrf_edgeai_input_t	     *p_input,
+						   nrf_edgeai_dsp_pipeline_t *p_dsp);
 /** @} */
 #ifdef __cplusplus
 }
--- include/nrf_edgeai/rt/private/features/nrf_edgeai_scale_features.h
+++ include/nrf_edgeai/rt/private/features/nrf_edgeai_scale_features.h
@@ -14,20 +14,22 @@ extern "C" {
 
 /**
  * @def NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE
- * @brief Macro to declare feature scaling interface functions for various data types and scaling modes.
+ * @brief Macro to declare feature scaling interface functions for various data types
+ * and scaling modes.
  *
  * All declared functions have the following prototype:
  * @code
- * nrf_edgeai_err_t nrf_edgeai_scale_features_<mode>_<input_type>_<output_type>(nrf_edgeai_input_t* p_input, nrf_edgeai_dsp_pipeline_t* p_dsp);
+ * nrf_edgeai_err_t nrf_edgeai_scale_features_<mode>_<input_type>_<output_type>(nrf_edgeai_input_t*
+ *      p_input, nrf_edgeai_dsp_pipeline_t* p_dsp);
  * @endcode
  * where:
  *   - \<mode\>        : input_vector, input_window, or dsp
  *   - \<input_type\>  : i8, i16, or f32 (input data type)
  *   - \<output_type\> : q8, q16, or f32 (output/target data type)
  */
-#define NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(interface_name)                                 \
-    nrf_edgeai_err_t nrf_edgeai_scale_features_##interface_name(nrf_edgeai_input_t*        p_input, \
-                                                                nrf_edgeai_dsp_pipeline_t* p_dsp)
+#define NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(interface_name)  \
+	nrf_edgeai_err_t nrf_edgeai_scale_features_##interface_name( \
+		nrf_edgeai_input_t *p_input, nrf_edgeai_dsp_pipeline_t *p_dsp)
 
 /**
  * @brief Scale a vector of input features to the target data type.
@@ -40,7 +42,8 @@ extern "C" {
  * @param[in, out] p_dsp          Pointer to the DSP pipeline context @ref nrf_edgeai_dsp_pipeline_t
  * @return Status code indicating success or error.
  *
- * @note Example functions: nrf_edgeai_scale_features_input_vector_i8_q8, nrf_edgeai_scale_features_input_vector_f32_q16, etc.
+ * @note Example functions: nrf_edgeai_scale_features_input_vector_i8_q8,
+ * nrf_edgeai_scale_features_input_vector_f32_q16, etc.
  */
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_vector_i8_q8);
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_vector_i8_q16);
@@ -57,13 +60,15 @@ NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_vector_f32_f32);
  *
  * These functions perform scaling of a window (matrix) of input features, where each row or column
  * represents a feature over a time window, from the original data type to the target type.
- * Scaling is performed using per-feature min/max values and supports selective scaling based on feature masks.
+ * Scaling is performed using per-feature min/max values and supports selective scaling based on
+ * feature masks.
  *
  * @param[in, out] p_input        Pointer to the input processing context @ref nrf_edgeai_input_t
  * @param[in, out] p_dsp          Pointer to the DSP pipeline context @ref nrf_edgeai_dsp_pipeline_t
  * @return Status code indicating success or error.
  *
- * @note Example functions: nrf_edgeai_scale_features_input_window_i8_q8, nrf_edgeai_scale_features_input_window_f32_f32, etc.
+ * @note Example functions: nrf_edgeai_scale_features_input_window_i8_q8,
+ * nrf_edgeai_scale_features_input_window_f32_f32, etc.
  */
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_window_i8_q8);
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_window_i8_q16);
@@ -78,15 +83,16 @@ NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(input_window_f32_f32);
 /**
  * @brief Scale DSP features from input data.
  *
- * These functions perform scaling of digital signal processing (DSP) features (such as statistical or spectral features)
- * from the input data.
- * The scaling is performed according to the configuration in the neural network context.
+ * These functions perform scaling of digital signal processing (DSP) features (such as statistical
+ * or spectral features) from the input data. The scaling is performed according to the
+ * configuration in the neural network context.
  *
  * @param[in, out] p_input        Pointer to the input processing context @ref nrf_edgeai_input_t
  * @param[in, out] p_dsp          Pointer to the DSP pipeline context @ref nrf_edgeai_dsp_pipeline_t
  * @return Status code indicating success or error.
  *
- * @note Example functions: nrf_edgeai_scale_features_dsp_i8_q8, nrf_edgeai_scale_features_dsp_f32_f32, etc.
+ * @note Example functions: nrf_edgeai_scale_features_dsp_i8_q8,
+ * nrf_edgeai_scale_features_dsp_f32_f32, etc.
  */
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(dsp_i8_q8);
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(dsp_i8_q16);
@@ -98,7 +104,7 @@ NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(dsp_f32_q8);
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(dsp_f32_q16);
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(dsp_f32_f32);
 
-// For models that do not require feature scaling, an empty implementation is provided.
+/* For models that do not require feature scaling, an empty implementation is provided. */
 NRF_EDGEAI_DECLARE_SCALE_FEATURES_INTERFACE(empty);
 
 #ifdef __cplusplus
